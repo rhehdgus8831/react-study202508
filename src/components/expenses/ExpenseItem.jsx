9cb78700deb4998714ca0a32b6245e819acb1ded
rhehdgus8831 <rhehdgus8831@naver.com>
2025-08-25 11:14:40 +0900

    1-4-2. props 기초

---
+++ src/components/expenses/ExpenseItem.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './ExpenseItem.css';
+
+const ExpenseItem = ({title,price,date}) => {
+
+    // console.log(props);
+
+    // const {title,price,date} = props;
+
+    return (
+        <div className='expense-item'>
+            <div>{date.toLocaleString()}</div>
+            <div className='expense-item__description'>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>{price}원</div>
+            </div>
+        </div>
+    );
+};
+
+export default ExpenseItem;
