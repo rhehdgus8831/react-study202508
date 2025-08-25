f523a8bbc8400ba0a58dd1e902a8609f83455a30
rhehdgus8831 <rhehdgus8831@naver.com>
2025-08-25 11:40:26 +0900

    1-4-5. props로 배열 전달하기

--- src/components/expenses/ExpenseList.jsx
+++ src/components/expenses/ExpenseList.jsx
@@ -2,25 +2,9 @@ import React from 'react';
 import './ExpenseList.css';
 import ExpenseItem from './ExpenseItem.jsx';
 
-const ExpenseList = () => {
+const ExpenseList = ({expenses: expenseList}) => {
 
-    const expenseList = [
-        {
-            title: '닭강정',
-            price: 8000,
-            date: new Date(2025, 7, 13)
-        },
-        {
-            title: '호두정과',
-            price: 50000,
-            date: new Date(2025, 8, 21)
-        },
-        {
-            title: '이펙티브 자바',
-            price: 30000,
-            date: new Date(2025, 4, 2)
-        },
-    ];
+
 
     return (
         <div className='expenses'>
