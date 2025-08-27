ab6b462b5f0cfe9665165d94d9ad8c079076120f
rhehdgus8831 <rhehdgus8831@naver.com>
2025-08-27 09:46:28 +0900

    4-1-1. 동적 인라인 스타일

--- src/components/CourseGoals/CourseInput.jsx
+++ src/components/CourseGoals/CourseInput.jsx
@@ -6,15 +6,15 @@ const CourseInput = ({onAdd}) => {
 
     const [enteredGoal, setEnteredGoal] = useState('');
 
-    const goalInputChangeHandler = e => {
-        setEnteredGoal(e.target.value);
-    }
+    // 입력값에 오류가 있는지 여부를 상태관리
+    const [isValid, setIsVaild] = useState(null);
 
 
     const handleSubmit = e => {
         e.preventDefault();
 
         if (enteredGoal.trim().length === 0) {
+            setIsVaild(false);
             return;
         }
         console.log(`입력된 목표`,enteredGoal);
@@ -23,17 +23,31 @@ const CourseInput = ({onAdd}) => {
 
 
         setEnteredGoal('');
+        setIsVaild(true)
     }
 
+    const goalInputChangeHandler = e => {
+        const inputValue = e.target.value;
+
+        if(inputValue.trim()) {
+            setIsVaild(true);
+        }
+
+        setEnteredGoal(inputValue);
+    }
 
     return (
         <form onSubmit={handleSubmit}>
             <div className='form-control'>
-                <label>나의 목표</label>
+                <label style={{color: isValid !== false ? 'black':'red'}}>나의 목표</label>
                 <input type='text'
                        value={enteredGoal}
                        onChange={goalInputChangeHandler}
-
+                       style=
+                           {{
+                           background : isValid !== false ? 'transparent': 'salmon',
+                           borderColor: isValid !== false ? 'black': 'red'
+                            }}
                 />
             </div>
             <Button type='submit'>목표 추가하기</Button>
