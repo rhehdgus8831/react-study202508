62cb1470d45dbddef6d589475f2f332a7c5a2a47
rhehdgus8831 <rhehdgus8831@naver.com>
2025-08-28 11:25:20 +0900

    6-1-2. 간단한 로그인 로직 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import MainHeader from './components/SideEffect/MainHeader.jsx';
 import Home from './components/SideEffect/Home.jsx';
@@ -6,13 +6,25 @@ import Login from './components/SideEffect/Login.jsx';
 
 const App = () => {
 
+    // 로그인 상태 체크
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const handleLogin = (email, password) => {
+
+        // 로그인 검증
+        if (email === 'abc@def.com' && password === '12345678') {
+            setIsLoggedIn(true);
+        } else {
+            alert('로그인 실패 !');
+        }
+    };
 
     return (
         <>
-            <MainHeader />
+            <MainHeader/>
             <main>
-                <Home />
-                <Login />
+                {isLoggedIn && <Home/>}
+                {!isLoggedIn && <Login onLogin={handleLogin}/>}
             </main>
         </>
     );
