adc34b6458ef05be36accd44350d0177f2726d18
rhehdgus8831 <rhehdgus8831@naver.com>
2025-08-28 14:08:53 +0900

    6장 복습예제. 3- 타이머 챌린지 컴퍼넌트 셋팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
 import Player from './components/timerGame/Player.jsx';
+import TimerChallenge from './components/timerGame/TimerChallenge.jsx';
 
 const App = () => {
 
     return (
         <>
             <Player/>
+            <div id="challenges">
+                <TimerChallenge/>
+            </div>
         </>
     )
 };
